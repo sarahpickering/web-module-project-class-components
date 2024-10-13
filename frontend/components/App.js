2208d956459f52324a2bcbe15ec9fb8220780b80
Sarah Pickering <sarahpsellsaz@gmail.com>
2024-10-13 14:29:58 -0700

    import Form and TodoList, create state to hold todos

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import Form from './Form'
+import TodoList from './TodoList'
+
+let id = 0
+let getId = () => ++id
+
+const initialTodos = [
+  { id: getId(), name: "Walk the dog", completed: false },
+  { id: getId(), nmae: "Learn React", completed: true },
+  { id: getId(), name: "Have fun", completed: false},
+]
+
 
 export default class App extends React.Component {
+    state = {
+      todos: initialTodos
+    }
   render() {
     return (
       <div>
-        Todo App
+        <TodoList />
+        <Form />
       </div>
     )
   }
