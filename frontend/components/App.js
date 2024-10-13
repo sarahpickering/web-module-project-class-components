b99d422caf6c0d30a39416040117b68746cd9718
Sarah Pickering <sarahpsellsaz@gmail.com>
2024-10-13 14:51:07 -0700

    add function for addTodo and toggleCompletion

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -16,10 +16,28 @@ export default class App extends React.Component {
     state = {
       todos: initialTodos
     }
+
+    addTodo = (name) => {
+      this.setState({
+        ...this.state,
+        todos: this.state.todos.concat({ id: getId(), completed: false, name})
+      })
+    }
+
+    toggleCompletion = id => {
+      this.setState({
+        ...this.state,
+        todos: this.state.todos.map(td => {
+          if (id == td.id) return { ...td, completed: !td.completed }
+          return td
+        })
+
+      })
+    }
   render() {
     return (
       <div>
-        <TodoList />
+        <TodoList todos={this.state.todos} />
         <Form />
       </div>
     )
